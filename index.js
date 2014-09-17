c24b7b7fcc7d8b473d4869f143f58c570b6acf94
Linus Unnebäck <linus@folkdatorn.se>
2014-09-17 11:44:22 +0200

    simplified the code base

--- index.js
+++ index.js
@@ -1,13 +1,4 @@
 'use strict';
 var got = require('got');
 
-module.exports = function (cb) {
-	got('http://api.ipify.org', function (err, res) {
-		if (err) {
-			cb(err);
-			return;
-		}
-
-		cb(null, res);
-	});
-};
+module.exports = got.bind(null, 'http://api.ipify.org');

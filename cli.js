1eb6753ddd18d8837c1deddb9c29cd15d44aa3dc
Arthur Verschaeve <contact@arthurverschaeve.be>
2015-05-19 18:52:08 +0200

    Use `meow` in CLI

--- cli.js
+++ cli.js
@@ -1,29 +1,15 @@
 #!/usr/bin/env node
 'use strict';
-var pkg = require('./package.json');
+var meow = require('meow');
 var ipify = require('./');
-var argv = process.argv.slice(2);
 
-function help() {
-	console.log([
-		'',
-		'  ' + pkg.description,
-		'',
-		'  Example',
-		'    ipify',
-		'    82.142.31.236'
-	].join('\n'));
-}
-
-if (argv.indexOf('--help') !== -1) {
-	help();
-	return;
-}
-
-if (argv.indexOf('--version') !== -1) {
-	console.log(pkg.version);
-	return;
-}
+meow({
+	help: [
+		'Example',
+		'  $ ipify',
+		'  82.142.31.236'
+	].join('\n')
+});
 
 ipify(function (err, ip) {
 	if (err) {

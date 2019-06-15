1670767b95fecff84f722e17d93557a7673dc2dd
Sindre Sorhus <sindresorhus@gmail.com>
2019-06-15 14:53:23 +0700

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,9 @@
 declare namespace ipify {
 	interface Options {
 		/**
-		Use IPv6 API endpoint. IPv6 endpoint will return IPv6 address if available, IPv4 address otherwise. Setting the `endpoint` option will override this.
+		Use the IPv6 API endpoint. The IPv6 endpoint will return an IPv6 address if available, IPv4 address otherwise.
+
+		Setting the `endpoint` option will override this.
 
 		@default true
 		*/
@@ -28,6 +30,7 @@ import ipify = require('ipify');
 (async () => {
 	console.log(await ipify());
 	//=> '2001:0db8:85a3:0000:0000:8a2e:0370:7334'
+
 	console.log(await ipify({useIPv6: false});
 	//=> '82.142.31.236'
 })();

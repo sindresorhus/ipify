17e64208f460c5f864f411b3b75e167924b11bbc
Will Macdonald <willmacdonald@users.noreply.github.com>
2020-09-14 00:39:38 +0200

    Fix readme typo (#14)
    Co-authored-by: Sindre Sorhus <sindresorhus@gmail.com>

--- index.d.ts
+++ index.d.ts
@@ -31,7 +31,7 @@ import ipify = require('ipify');
 	console.log(await ipify());
 	//=> '2001:0db8:85a3:0000:0000:8a2e:0370:7334'
 
-	console.log(await ipify({useIPv6: false});
+	console.log(await ipify({useIPv6: false}));
 	//=> '82.142.31.236'
 })();
 ```
